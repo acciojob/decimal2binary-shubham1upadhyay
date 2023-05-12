929a5f8d48c9281142f69f43006249104f088c29
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-12 17:12:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function decimalToBinary(decimal) {
+let a = document.getElementById("decimal").value;
+function decimalToBinary(a) {
   let binary = "";
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
+  while (a > 0) {
+    binary = (a % 2) + binary;
+    a = Math.floor(a / 2);
   }
   // return binary;
 	document.getElementById("ans").innerHTML = binary;
 }
 
 
-module.exports = threeSum;
+module.exports = decimalToBinary;
