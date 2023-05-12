131dc3527d68b9717a6d60e4f6e212fe7fc85760
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-12 16:49:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function decimalToBinary(decimal) {
+  let binary = "";
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
 }
+
 
 module.exports = threeSum;
