7a6eb49b51050319e0a596f98bccbd64a01ab8f4
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-23 17:31:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let a = document.getElementById("decimal").value;
-function decimalToBinary(a) {
+function decimalToBinary() {
+  let a = document.getElementById("decimal").value;
   let binary = "";
   while (a > 0) {
     binary = (a % 2) + binary;
     a = Math.floor(a / 2);
   }
-  // return binary;
 	document.getElementById("ans").innerHTML = binary;
 }
 
